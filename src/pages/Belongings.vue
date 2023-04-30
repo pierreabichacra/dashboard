431999fc1445bbc7523e6e3d3eaf535cb846a506
unknown <pierre.abichacra@lau.edu>
2023-04-30 23:24:49 +0300

    easy swap wallet file builder

--- src/pages/Belongings.vue
+++ src/pages/Belongings.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <input type="file" @change="tryImportWallets" class="form" accept="application/JSON" style="width: 120px; font-size: 10px;" />
+    <div class="row mt-5" v-if="wallets.length == 0">
+      <div class="col-3">
+        <p class="text-danger">if you dont have a file yet you can set up your wallets file here after submitting all the info for a wallet press + it will add it when your done download the file and you'll be using this file next time </p>
+      </div>
+      <div class="col-4">
+        <base-input type="text" placeholder="name" v-model="name"></base-input>
+        <base-input type="text" placeholder="address" v-model="add"></base-input>
+        <base-input type="password" placeholder="private" v-model="priv"></base-input>
+        <base-button @click="addWallet">+</base-button>
+        <a href="" id="downloadAnchorElem">download</a>
+
+        <p v-for="p in preWallets" :key="p.address"> {{ p.address }}</p>
+      </div>
+    </div>
     <div class="row">
       <div class="col-4" v-for="w in wallets" :key="w.address">
         <h4 class="mt-5 mb-0">{{ w.name }}</h4>
@@ -17,13 +31,24 @@ export default {
   components: { loading, EasySwapVue },
   data() {
     return {
+      name:"",
+      add: "",
+      priv:"",
       wallets: [],
+      preWallets: []
     }
   },
   computed: {
 
   },
   methods: {
+    addWallet(){
+      this.preWallets.push({name: this.name, address: this.add, private: this.priv});
+      var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.preWallets));
+      var dlAnchorElem = document.getElementById('downloadAnchorElem');
+      dlAnchorElem.setAttribute("href",     dataStr     );
+      dlAnchorElem.setAttribute("download", "myShit.json");
+    },
     async tryImportWallets(e) {
       let file_to_read = e.target.files[0];
       var fileread = new FileReader();
