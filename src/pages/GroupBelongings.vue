5a564a253bf491e261d5558ece48081c2e3e687e
unknown <pierre.abichacra@lau.edu>
2023-08-02 17:36:30 +0300

    addedyoken filter

--- src/pages/GroupBelongings.vue
+++ src/pages/GroupBelongings.vue
@@ -1,39 +1,9 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col" v-if="chartToken">
-        <base-button @click="refreshChart"><i class="tim-icons icon-refresh-01"></i></base-button>
-        <base-button @click="chartToken = null"><i class="tim-icons icon-simple-remove"></i></base-button>
-        <div class="text-center" style="overflow-y: scroll; height: 810px;" v-if="chartRefresh">
-          <iframe :src="`https://dexscreener.com/ethereum/${chartToken}`" frameborder="0" width="100%"
-            height="800px"></iframe>
-        </div>
-
-      </div>
-    </div>
-    <input type="file" @change="tryImportWallets" class="form" accept="application/JSON"
-      style="width: 120px; font-size: 10px;" v-if="wallets.length == 0" />
-    <div class="row mt-5" v-if="wallets.length == 0">
-      <div class="col-3">
-        <p class="text-danger">if you dont have a file yet you can set up your wallets file here after submitting all the
-          info for a wallet press + it will add it when your done download the file and you'll be using this file next
-          time </p>
-      </div>
-      <div class="col-4">
-        <base-input type="password" class="mb-5" placeholder="encryption password" v-model="enc"></base-input>
-
-        <base-input type="text" placeholder="name" v-model="name"></base-input>
-        <base-input type="text" placeholder="address" v-model="add"></base-input>
-        <base-input type="password" placeholder="private" v-model="priv"></base-input>
-        <base-button @click="addWallet">+</base-button>
-        <a href="" id="downloadAnchorElem">download</a>
-
-        <p v-for="p in preWallets" :key="p.address"> {{ p.address }}</p>
-      </div>
-    </div>
+  <div>   
     <br>
-    <div v-if="wallets.length > 0">
+    <div class="row" v-if="wallets.length > 0">
       <base-button @click="guiRefresh = !guiRefresh">double click to reset all wallets</base-button>
+      <base-button @click="trackedProject +=1">+ [token]</base-button>
       <base-button @click="refreshGas">{{ currentGwei }}</base-button>
       <div class="row">
         <div class="col">
@@ -51,8 +21,8 @@
         </div>
       </div>
     </div>
-
-    <div class="row" v-if="guiRefresh">
+    <div v-for="t in trackedProject" :key="t">
+      <div class="row" v-if="guiRefresh">
       <div class="col-4" v-for="w in wallets" :key="w.address">
         <h5 class="mt-5 mb-0" v-if="token_owners.includes(w.address)"><a target="_blank" :href="`https://etherscan.io/address/${w.address}`">{{ w.name }}</a>
         </h5>
@@ -60,25 +30,29 @@
         <EasySwapVue :_address="w.address" :_private="w.private" v-if="token_owners.includes(w.address)" :currentToken="token_owners.includes(w.address) ? token_address : null"/>
       </div>
     </div>
+    </div>
+   
   </div>
 </template>
 <script>
 import loading from "@/pages/custom_components/loading.vue";
 import EasySwapVue from './EasySwap.vue';
 import Web3 from "web3";
-import GroupBelongings from "./GroupBelongings.vue";
+
 export default {
-  components: { loading, EasySwapVue, GroupBelongings },
+  components: { loading, EasySwapVue },
+  props: {
+    wallets: {type: Array}
+  },
   data() {
     return {
-      trackedProject: 0,
+      trackedProject: 1,
       chartToken: "",
       guiRefresh: true,
       enc: "",
       name: "",
       add: "",
       priv: "",
-      wallets: [],
       preWallets: [],
       currentGwei: 0,
       web3: null,
