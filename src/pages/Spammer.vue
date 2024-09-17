2c4089b762d296ff1db4a4d304df80bf57fe3a91
Pierre Abi chacra <pierreabichacra@Pierres-MBP.lan>
2024-09-17 03:30:07 +0200

    added a spammer with bribe functionality

--- src/pages/Spammer.vue
+++ src/pages/Spammer.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="container">
-    <div v-if="token.name">
-      <h5>{{ token.name }} / {{ token.symbol }}</h5>
-      <h5>total supply: {{ token.totalSupply }}</h5>
-      <h5>decimals: {{ token.decimals }} </h5>
+    <base-input :label="`RPC`" placeholder="RPC" @click="changeNode" v-model="currentChosenRPC"></base-input>
+
+    <div>
+      <h5>{{ token.name }} {{ token.name ? '/ ' : '' }} {{ token.symbol }}</h5>
+      <h5>{{ token.totalSupply ? `total supply: ${token.totalSupply}` : '' }}</h5>
+      <h5>{{ token.decimals ? `decimals: ${token.decimals}` : '' }} </h5>
     </div>
 
     <base-input :label="`token address`" placeholder="token address" @click="pasteContract" v-model="token_address">
@@ -11,15 +13,21 @@
     </base-input>
 
     <div class="row d-flex align-items-center my-3">
-      <div class="col-5">
+      <div class="col-3">
         <base-input :label="`Max TX`" placeholder="Max TX" v-model="max_tx" @change="changeMaxTxTokens"></base-input>
       </div>
       <div class="col-2">
+        <base-input :label="`GWEI extra`" placeholder="GWEI" v-model="gweiUsed"></base-input>
+        <base-input :label="`Gas Limit`" placeholder="GAS LIMIT" v-model="gasLimitUsed"></base-input>
       </div>
       <div class="col-2">
-        {{ valueEth }} ETH
+        <base-input step="0.1" type="number" :label="`ETH value`" placeholder="eth value"
+          v-model="eth_value"></base-input>
       </div>
-      <div class="col-3">
+      <div class="col-2">
+        <base-input step="0.1" type="number" :label="`TIP value`" placeholder="tip value" v-model="tip"></base-input>
+      </div>
+      <div class="col-2">
         <base-input type="number" min="0" :label="`token % `" placeholder="token %" v-model="max_tx_percent"
           @change="changeMaxTxPercent">
         </base-input>
@@ -28,8 +36,21 @@
 
 
     <div class="easyswapcontainer d-flex align-items-center my-3 row" v-if="recipients.length > 0">
+      <small class="text-success">{{ recipients_address }}</small>
       <div class="col-12" v-for="(r, i) in recipients" :key="i" :label="`recipient ${i + 1}`">
-        <label for="">{{ r.name }}</label><base-input v-model="r.address" disabled></base-input>
+        <div class="row d-flex align-items-center">
+          <div class="col-1">
+            <label for="">{{ r.name }}</label>
+          </div>
+          <div class="col-10">
+            <base-input v-model="r.address" disabled></base-input>
+          </div>
+          <div class="col-1">
+            <base-input type="checkbox" @change="addRemoveWallet(r.address)" style="width='50%'"
+              v-if="i != 0"></base-input>
+
+          </div>
+        </div>
       </div>
     </div>
 
@@ -46,8 +67,15 @@
     </div>
 
     <div class="row mt-5">
-      <base-button type="primary" @click="startSpamming">start spamming</base-button>
-      <base-button type="primary" @click="stopInterval" v-if="intervalID != -1">stop</base-button>
+      <div class="col-12 col-sm-6">
+        <base-button type="primary" @click="startSpamming">start spamming</base-button>
+        <base-button type="primary" @click="stopInterval" v-if="intervalID != -1">stop</base-button>
+
+      </div>
+      <div class="col-12 col-sm-6">
+        <textarea v-model="infoText" name="" id="" rows="10"
+          style="width: 100%; background-color: black; color: limegreen;"></textarea>
+      </div>
 
 
     </div>
@@ -57,6 +85,7 @@
 import Web3 from "web3";
 import TokenABI from "@/ABIS/Token.json";
 import UniswapABI from "@/ABIS/UniswapABI.json"
+import SPAM_ABI from "@/ABIS/SpammiosABI.json"
 
 import loading from "@/pages/custom_components/loading.vue";
 import { ethers, BigNumber, providers, Transaction, FixedNumber } from "ethers";
@@ -73,20 +102,29 @@ export default {
   },
   data() {
     return {
+      tip: 0,
+      infoText: "",
+      eth_value: 0,
       web3: null,
       httpsProvider: null,
       intervalID: -1,
       currentBlock: 0,
       transactionsSent: 0,
+      gweiUsed: 20,
+      currentGwei: 0,
+      gasLimitUsed: 1000000,
       enc: "",
       max_tx_percent: "",
       max_tx: "",
-      valueEth: 0,
       token: {},
       token_address: "",
       recipients: [],
+      recipients_address: [],
       weth_address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       uniswapRouterAddress: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      spammiosAddress: "0xE61053A500877a8F26673CBd548A1F35EcC8ac08",
+      wallet: null,
+      currentChosenRPC: "",
     }
   },
   computed: {},
@@ -105,19 +143,64 @@ export default {
 
       // this.intervalID = setInterval(this.checkBlockAndSendTx(), 2000);
     },
-    
+    async refreshGas() {
+      this.currentGwei = 0;
+      this.currentGwei = await this.web3.eth.getGasPrice();
+      console.log(this.currentGwei)
+    },
+    addRemoveWallet(address) {
+      for (let i = 0; i < this.recipients_address.length; i++) {
+        if (this.recipients_address[i] == this.web3.utils.toChecksumAddress(address)) {
+          this.recipients_address.splice(i, 1);
+          this.danger("removed " + address)
+          return;
+        }
+      }
+      this.recipients_address.push(this.web3.utils.toChecksumAddress(address))
+      this.success("added " + address)
+    },
+
+    async prpepareRawTxData() {
+      var contract = new this.web3.eth.Contract(SPAM_ABI, this.web3.utils.toChecksumAddress(this.spammiosAddress), { from: this.web3.utils.toChecksumAddress(this.recipients[0].address) });
+      var ethAmountToBuyWith = this.web3.utils.toWei((Number(this.eth_value) + Number(this.tip)).toString(), 'ether');
+      let tipAmount = BigNumber.from(this.web3.utils.toWei(this.tip, 'ether'));
+      ethAmountToBuyWith = BigNumber.from(ethAmountToBuyWith);
+      console.log(ethAmountToBuyWith)
+      let maxTxTokens = BigNumber.from(Number(this.max_tx)).mul(BigNumber.from(10).pow(BigNumber.from(this.token.decimals)))
+      let path = [this.web3.utils.toChecksumAddress(this.weth_address), this.web3.utils.toChecksumAddress(this.token_address)]
+
+      var data = contract.methods.BuyMany(maxTxTokens._hex, path, this.recipients_address, tipAmount._hex);
+      var count = await this.getCurrentNonce(this.web3.utils.toChecksumAddress(this.recipients[0].address));
+      console.log(count)
+      await this.refreshGas()
+      var rawTransaction = {
+        "from": this.recipients[0].address,
+        "maxFeePerGas": this.web3.utils.toHex(Number(this.currentGwei) + Number(this.web3.utils.toWei(this.gweiUsed, 'gwei'))),
+        "maxPriorityFeePerGas": this.web3.utils.toHex(Number(this.currentGwei) + Number(this.web3.utils.toWei(this.gweiUsed, 'gwei'))),
+        "gasLimit": this.web3.utils.toHex(this.gasLimitUsed),
+        "to": this.spammiosAddress,
+        "value": ethAmountToBuyWith._hex,
+        "data": data.encodeABI(),
+        "nonce": this.web3.utils.toHex(`${count}`),
+        "type": 2,
+        "chainId": 1,
+      };
+      console.log(rawTransaction)
+      this.info(`gwei: ${this.gweiUsed}\ngasLimit: ${this.gasLimitUsed}\nvalue: ${this.eth_value} ETH\ntoken amount: ${this.max_tx} \n eth value ${this.eth_value} ETH\n`)
+      this.info(`sending tx`);
+      await this.signAndSendEIP1559Transaction(rawTransaction);
+    },
+
     async checkBlockAndSendTx() {
       let self = this;
       console.log(this.currentBlock)
-      this.web3.eth.getBlockNumber(function (error, curBlk) {
+      this.web3.eth.getBlockNumber(async function (error, curBlk) {
         self.info("newest block is " + curBlk)
         console.log("cheking block " + curBlk)
         if (curBlk > self.currentBlock) {
           self.info("sending tx for block " + curBlk);
-          for (let i = 0; i < self.recipients.length; i++) {
-            self.success(`tx sent for ${self.recipients[i].name}`);
-          }
           self.currentBlock = curBlk
+          await self.prpepareRawTxData();
           return curBlk
         }
       });
@@ -133,6 +216,14 @@ export default {
           const decryptedText = this.$CryptoJS.AES.decrypt(content, "_1_" + this.enc).toString(this.$CryptoJS.enc.Utf8)
           var intern = JSON.parse(decryptedText); // Array of Objects.
           this.recipients = intern;
+          this.recipients_address.push(this.recipients[0].address)
+          for (let i = 0; i < intern.length; i++) {
+            if (i == 0) {
+              this.wallet = (new ethers.Wallet(intern[i].private.toString('hex'), this.httpsProvider));
+            }
+            delete this.recipients[i].private;
+          }
+
         } catch (e) {
           console.error(e)
           this.$notify({
@@ -148,29 +239,6 @@ export default {
       fileread.readAsText(file_to_read);
 
     },
-    async getMaxTxPrice() {
-      try {
-        var contract = new this.web3.eth.Contract(TokenABI, this.token_address)
-        let tokensWithDecimals = BigNumber.from(this.max_tx).mul(BigNumber.from(BigNumber.from(10).pow(this.token.decimals)));
-        contract = new this.web3.eth.Contract(UniswapABI, this.uniswapRouterAddress, { from: this.walletAddress });
-        let value_in_eth = await contract.methods.getAmountsOut(
-          tokensWithDecimals,
-          [this.token_address, this.weth_address]
-        ).call();
-        tokensWithDecimals = value_in_eth.length >= 0 ? value_in_eth[1] : 0;
-        console.log(ethers.utils.formatEther(value_in_eth[1]))
-        this.valueEth = ethers.utils.formatEther(value_in_eth[1]);
-      } catch (e) {
-        console.error(e)
-        this.$notify({
-          type: "danger",
-          timeout: 1500,
-          message: e.toString()
-        });
-        this.valueEth = "error"
-        return 0;
-      }
-    },
     async pasteContract() {
       this.token_address = await navigator.clipboard.readText();
       await this.getContractInfo();
@@ -182,7 +250,6 @@ export default {
         return
       }
       this.max_tx = this.token.totalSupply * this.max_tx_percent / 100;
-      this.getMaxTxPrice();
     },
     changeMaxTxTokens() {
       this.max_tx_percent = 0;
@@ -210,22 +277,27 @@ export default {
         });
       }
     },
-    addRecipients() {
-      this.recipients.push("")
-    },
-    async signAndSendEIP1559Transaction(rawTransaction, priv) {
+
+    async signAndSendEIP1559Transaction(rawTransaction) {
       // to do the flash bot tx check this link https://docs.flashbots.net/flashbots-auction/searchers/advanced/private-transaction
       try {
-        let wallet = new ethers.Wallet(priv.toString('hex'), this.httpsProvider);
-        let tx = rawTransaction;
-        tx.chainId = 1;
-        tx.type = 2;
-        const signedTx = await wallet.signTransaction(rawTransaction);
+        const signedTx = await this.wallet.signTransaction(rawTransaction);
         const txHash = ethers.utils.keccak256(signedTx);
-        this.info("sending");
-        let x = await this.httpsProvider.sendTransaction(signedTx);
+        this.info("sending \n" + txHash);
+        let tx = await this.httpsProvider.sendTransaction(signedTx);
         window.open(`https://etherscan.io/tx/${txHash}`, '_blank')
-        this.success("tx mined");
+        tx.wait().then(receipt => {
+          if (receipt.status === 1) {
+            this.success('Transaction succeeded:', receipt.hash);
+            this.stopInterval();
+          } else {
+            this.danger('Transaction reverted:', receipt.hash);
+          }
+        }).catch(error => {
+          this.danger(error);
+          console.error('Error:', error);
+        });
+
       } catch (e) {
         console.warn(e);
         this.danger(`Tx erored:  ${e.message}`);
@@ -259,6 +331,8 @@ export default {
       return this.nonce;
     },
     danger(text) {
+      this.infoText += "\n " + text;
+
       this.$notify({
         type: "danger",
         timeout: 2000,
@@ -266,6 +340,8 @@ export default {
       });
     },
     success(text) {
+      this.infoText += "\n " + text;
+
       this.$notify({
         type: "success",
         timeout: 2000,
@@ -273,6 +349,7 @@ export default {
       });
     },
     info(text) {
+      this.infoText += "\n " + text;
       this.$notify({
         type: "info",
         timeout: 2000,
@@ -288,9 +365,23 @@ export default {
       this.isLoading = false;
     },
 
+    changeNode(){
+      this.web3 = new Web3(this.currentChosenRPC);
+      this.httpsProvider = new ethers.providers.JsonRpcProvider(this.currentChosenRPC);
+      localStorage.setItem("rpc", this.currentChosenRPC);
+
+    },
+
   },
   mounted() {
-    let publicNode = "https://rpc.mevblocker.io/noreverts";
+    let publicNode;
+    if(localStorage.getItem("rpc")){
+      publicNode = localStorage.getItem("rpc");
+      this.currentChosenRPC = localStorage.getItem("rpc");
+    }else{
+      publicNode = "https://eth.drpc.org";
+      this.currentChosenRPC = publicNode
+    }
     this.web3 = new Web3(publicNode);
     this.httpsProvider = new ethers.providers.JsonRpcProvider(publicNode);
     this.info(`using ${publicNode}`)
